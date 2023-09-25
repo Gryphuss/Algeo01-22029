ef28f0cdaf1eaf477a1642c66e0b0d4ac82cb9dc
Gryphuss <116566870+Gryphuss@users.noreply.github.com>
2023-09-25 20:36:33 +0700

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,127 +1,93 @@
 import java.util.*;
 
 public class Matrix {
-    private static final int ROW_CAP = 100; // Unnecessarily 100
+    private static final int ROW_CAP = 100;
     private static final int COL_CAP = 100;
     // Final means unchangeable
 
-    private int[][] mem;
+    private double[][] matrx;
     private int rowEff;
     private int colEff;
 
     public Matrix(int nRows, int nCols) {
-
+        this.matrx = new double[ROW_CAP][COL_CAP];
+        this.rowEff = nRows;
+        this.colEff = nCols;
     }
 
-    public int getRowEff() {
-
-    }
-
-    public int getColEff() {
-
-    }
-
-    public int getElement(int i, int j) {
-        
-    }
-
-    public boolean isMatrixIdxValid(int i, int j) {
-        
-    }
-
-    public int getLastIdxRow() {
-        
-    }
-
-    public int getLastIdxCol() {
-        
-    }
-
-    public boolean isIdxEff(int i, int j) {
-        
-    }
-
-    public int getElmtDiagonal(int i) {
-        
-    }
-
+    // 60% not sure if this will work
     public void copyMatrix(Matrix mIn) {
-        
+        this.rowEff = mIn.rowEff;
+        this.colEff = mIn.colEff;
+        this.matrx = new double[mIn.rowEff][mIn.colEff];
+        for (int i = 0; i < mIn.rowEff; i++) {
+            for (int j = 0; j < mIn.colEff; j++) {
+                this.matrx[i][j] = mIn.matrx[i][j];
+            }
+        }
     }
 
     public static Matrix addMatrix(Matrix m1, Matrix m2) {
-        
+        Matrix after = new Matrix(m1.rowEff, m1.colEff);
+        for (int i = 0; i < m1.rowEff; i++) {
+            for (int j = 0; j < m1.colEff; j++) {
+                after.matrx[i][j] = m1.matrx[i][j] + m2.matrx[i][j];
+            }
+        }
+        return after;
     }
 
     public static Matrix subtractMatrix(Matrix m1, Matrix m2) {
-        
+        Matrix after = new Matrix(m1.rowEff, m1.colEff);
+        for (int i = 0; i < m1.rowEff; i++) {
+            for (int j = 0; j < m1.colEff; j++) {
+                after.matrx[i][j] = m1.matrx[i][j] - m2.matrx[i][j];
+            }
+        }
+        return after;
     }
 
     public static Matrix multiplyMatrix(Matrix m1, Matrix m2) {
-        
-    }
-
-    public static Matrix multiplyMatrixWithMod(Matrix m1, Matrix m2, int mod) {
-        
+        Matrix after = new Matrix(m1.rowEff, m2.colEff);
+        for (int i = 0; i < after.rowEff; i++) {
+            for (int j = 0; j < after.colEff; j++) {
+                for (int k = 0; k < m1.colEff; k++) {
+                    after.matrx[i][j] += m1.matrx[i][k] * m2.matrx[k][j];
+                }
+            }
+        }
+        return after;
     }
 
     public static Matrix multiplyByConst(Matrix m, int x) {
-        
-    }
-
-    public void pMultiplyByConst(int k) {
-        
-    }
-
-    public static boolean isMatrixEqual(Matrix m1, Matrix m2) {
-        
-    }
-
-    public static boolean isMatrixNotEqual(Matrix m1, Matrix m2) {
-        
-    }
-
-    public static boolean isMatrixSizeEqual(Matrix m1, Matrix m2) {
-        
-    }
-
-    public int countElmt() {
-        
+        Matrix after = new Matrix(m.rowEff, m.rowEff);
+        for (int i = 0; i < after.rowEff; i++) {
+            for (int j = 0; j < after.colEff; j++) {
+                after.matrx[i][j] *= x;
+            }
+        }
+        return after;
     }
 
     public boolean isSquare() {
-        
+        return (this.rowEff == this.colEff);
     }
 
     public boolean isSymmetric() {
-        
+        // Use one of the determinant checking method later
     }
 
     public boolean isIdentity() {
-    
-    }
-
-    public boolean isSparse() {
-        
-    }
-
-    public Matrix negation() {
-        
-    }
-
-    public void pNegation() {
-        
-    }
-
-    public int determinant() {
-        
+        // MALASSSSSSSSSSSSSSSS
     }
 
     public Matrix transpose() {
-        
-    }
-
-    public void pTranspose() {
-        
-    }
+        Matrix after = new Matrix(this.colEff, this.rowEff);
+        for (int i = 0; i < this.colEff; i++) {
+            for (int j = 0; j < this.rowEff; j++) {
+                after.matrx[i][j] = this.matrx[j][i];
+            }
+        }
+        return after;
+    } 
 }
