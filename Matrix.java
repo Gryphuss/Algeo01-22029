1c94fcdb1b421e83eeddc6657829dab37401f29f
Gryphuss <116566870+Gryphuss@users.noreply.github.com>
2023-09-25 19:28:43 +0700

    REMAKE

--- Matrix.java
+++ Matrix.java
@@ -1,58 +1,127 @@
-import java.util.Scanner;
-public class Matrix{
+import java.util.*;
 
-    private float[][] m;
-    private int nRow;
-    private int nCol;
+public class Matrix {
+    private static final int ROW_CAP = 100; // Unnecessarily 100
+    private static final int COL_CAP = 100;
+    // Final means unchangeable
+
+    private int[][] mem;
+    private int rowEff;
+    private int colEff;
+
+    public Matrix(int nRows, int nCols) {
+
+    }
+
+    public int getRowEff() {
+
+    }
+
+    public int getColEff() {
+
+    }
+
+    public int getElement(int i, int j) {
+        
+    }
+
+    public boolean isMatrixIdxValid(int i, int j) {
+        
+    }
+
+    public int getLastIdxRow() {
+        
+    }
+
+    public int getLastIdxCol() {
+        
+    }
+
+    public boolean isIdxEff(int i, int j) {
+        
+    }
+
+    public int getElmtDiagonal(int i) {
+        
+    }
+
+    public void copyMatrix(Matrix mIn) {
+        
+    }
+
+    public static Matrix addMatrix(Matrix m1, Matrix m2) {
+        
+    }
+
+    public static Matrix subtractMatrix(Matrix m1, Matrix m2) {
+        
+    }
+
+    public static Matrix multiplyMatrix(Matrix m1, Matrix m2) {
+        
+    }
+
+    public static Matrix multiplyMatrixWithMod(Matrix m1, Matrix m2, int mod) {
+        
+    }
+
+    public static Matrix multiplyByConst(Matrix m, int x) {
+        
+    }
+
+    public void pMultiplyByConst(int k) {
+        
+    }
+
+    public static boolean isMatrixEqual(Matrix m1, Matrix m2) {
+        
+    }
+
+    public static boolean isMatrixNotEqual(Matrix m1, Matrix m2) {
+        
+    }
+
+    public static boolean isMatrixSizeEqual(Matrix m1, Matrix m2) {
+        
+    }
+
+    public int countElmt() {
+        
+    }
+
+    public boolean isSquare() {
+        
+    }
+
+    public boolean isSymmetric() {
+        
+    }
+
+    public boolean isIdentity() {
     
-    
-    //konstruktor
-    public Matrix(){
-        m = new float[1000][1000]; //matrix 1000x1000
-        for(int i=0; i<1000; i++){
-            for(int j=0; j<1000; j++){
-                m[i][j]=-9999;
-            }
-        }
-    }
-    //getter
-    public float ELMT(Matrix m, int i, int j){
-        return this.m[i][j];
-    }
-    public int getRow(){
-        return nRow;
-    }
-    public int getCol(){
-        return nCol;
     }
 
-    //setter
-    public void setRow(int row){
-        this.nRow = row;
-    }
-    public void setCol(int col){
-        this.nCol = col;
-    }
-    public void setMtx(int x, int y){
-        this.nRow = x;
-        this.nCol = y;
-        for( int i = 0; i<nRow ; i++){
-            for( int j = 0; j<nCol; j++){
-                Scanner masukan = new Scanner (System.in);
-                float f = masukan.nextFloat();
-                this.m[i][j] = f;
-            }
-        }
+    public boolean isSparse() {
+        
     }
 
-    //display
-    public void printMtx(){
-        for(int i = 0; i < nRow; i++){
-            for(int j = 0; j < nCol-1; j++){
-                System.out.print(this.m[i][j]);
-                System.out.print(" ");
-            }
-            System.out.println(this.m[i][nCol-1]);
-        }
+    public Matrix negation() {
+        
+    }
+
+    public void pNegation() {
+        
+    }
+
+    public int determinant() {
+        
+    }
+
+    public Matrix transpose() {
+        
+    }
+
+    public void pTranspose() {
+        
     }
 }
