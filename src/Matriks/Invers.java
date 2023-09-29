de71232caf462cc3f1a8ebb614fd84394b825db7
Bbennn <sihombingbenjamin2004@gmail.com>
2023-09-29 19:03:19 +0700

    update invers

--- src/Matriks/Invers.java
+++ src/Matriks/Invers.java
@@ -4,6 +4,25 @@ import java.util.Scanner;
 
 public class Invers{
 
+    public Matriks Invers(Matrix m){
+        m = m.setUpInvers(m);
+        m = m.takeInversInvers(m);
+
+        if(inputMode == 1){
+            writeMatrix(m);
+        }else{
+            System.out.println("Matriks balikan dari matrix awal:\n");
+            for(int i=0; i<m.getRow(); i++){
+                for(int j=0; j<m.getCol(); j++){
+                    System.out.print(m[i][j]);
+                    System.out.print(" ");
+                }
+                System.out.print("\n");
+
+            }
+        }
+    }
+    
     public Matrix setUpInvers(Matrix m){
         Matrix mI = new Matrix(m.getRow(),2*m.getCol());
         for(int i=0; i<m.getRow(); i++){
