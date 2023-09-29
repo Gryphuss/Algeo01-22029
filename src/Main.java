068e5e8a05c1e9fb1ba0955c9c5832ddcf410264
Ignatius Jhon Hezkiel Chan <kielchanyeol@gmail.com>
2023-09-29 15:48:04 +0700

    Nyesuain

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        Matrix m = new Matrix(4, 5);
-        m.readMatrix();
-        OBE.GaussJordan(m);
-        m.displayMatrix();
-    }
-}
