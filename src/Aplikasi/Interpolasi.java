67cc2c5ee4ec2387e51691324e4080613e2d13c2
Gryphuss <116566870+Gryphuss@users.noreply.github.com>
2023-10-01 15:55:25 +0700

    Update Interpolasi.java

--- src/Aplikasi/Interpolasi.java
+++ src/Aplikasi/Interpolasi.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import Matriks.Matrix;
 import Matriks.OBE;
+import IO.*;
 
 public class Interpolasi {
     public static Scanner input = new Scanner(System.in);
@@ -19,6 +20,24 @@ public class Interpolasi {
                     fin.mem[i][j] = firstIn.mem[i][1];
                 } else {
                     fin.mem[i][j] = Math.pow(firstIn.mem[i][0], j);
+                }
+            }
+        }
+
+        return fin;
+    }
+
+    public static Matrix inToMatrixv2(Matrix m) {
+        Matrix fin = new Matrix(m.getRow(), m.getRow() + 1);
+        // Matrix firstIn = new Matrix(m., 2);
+        // firstIn.readMatrix();
+
+        for (int i = 0; i < m.getRow(); i++) {
+            for (int j = 0; j < m.getRow() + 1; j++) {
+                if (j == m.getRow()) {
+                    fin.mem[i][j] = m.mem[i][1];
+                } else {
+                    fin.mem[i][j] = Math.pow(m.mem[i][0], j);
                 }
             }
         }
@@ -42,7 +61,7 @@ public class Interpolasi {
                 } else if (i == 1) {
                     out += (m.mem[i][m.getCol() - 1] <= 0 ? " - " + nega : " + " + posi) + "x";
                 } else if (i == m.getRow() - 1) {
-                    out += (m.mem[i][m.getCol() - 1] <= 0 ? " - " + nega : " " + posi) + "x^" + i;
+                    out += (m.mem[i][m.getCol() - 1] <= 0 ? " - " + nega : " + " + posi) + "x^" + i;
                 } else {
                     out += (m.mem[i][m.getCol() - 1] <= 0 ? " - " + nega : " + " + posi) + "x^" + i;
                 }
@@ -70,14 +89,14 @@ public class Interpolasi {
         int selecmed = 0;
         boolean nonvalid = true;
 
-        Matrix matr = new Matrix(0, 0);
-        // Matrix matrPoints = new Matrix();
-        double x = 0;
-        // double[] hasily = new double[];
+        Matrix matr = new Matrix(100, 100);
+        Matrix matrPoints = new Matrix(100, 100);
+        Matrix rightside = new Matrix(1, 1);
+        // double rightside = 0;
 
-        selecmed = input.nextInt();
         // System.out.println();
         while (nonvalid) {
+            selecmed = input.nextInt();
             switch (selecmed) {
                 case 1:
                     System.out.println("Masukkan jumlah pasangan titik: ");
@@ -86,24 +105,35 @@ public class Interpolasi {
                     System.out.println("Masukkan titik-titik yang diinginkan: ");
                     // Matrix matr = new Matrix(n, n + 1);
                     matr = inToMatrix(n);
+                    // matr.displayMatrix();
                     System.out.println("Masukkan nilai x yang ingin dicari hasilnya: ");
-                    x = input.nextDouble();
+                    rightside.mem[0][0] = input.nextDouble();
                     nonvalid = false;
                     break;
-                // case 2:
-                // default:
+                case 2:
+                    // System.out.println("Masukkan nama file yang diinginkan: ");
+                    IO.IOput.readFileToMatrixInterpolasi(matrPoints, rightside);
+                    // System.out.println(rightside.mem[0][0]);
+
+                    // matrPoints.displayMatrix();
+                    matr = inToMatrixv2(matrPoints);
+                    // matr.displayMatrix();
+                    nonvalid = false;
+                    break;
+                default:
+                    System.out.println("Input tidak valid, masukkan hanya 1 atau 2!");
             }
         }
         if (!nonvalid) {
             Matrix finale = MatrixInterpolasi(matr);
-
+            // finale.displayMatrix();
             String hasilpoli = InterpolasiPrint(finale);
             System.out.println("Polinom yang melalui titik-titik tersebut adalah: ");
             System.out.println(hasilpoli);
 
             String finalres = hasilpoli;
 
-            String hasily = HasilYPrint(finale, x);
+            String hasily = HasilYPrint(finale, rightside.mem[0][0]);
             System.out.println("Nilai y dari nilai x yang diinginkan adalah: ");
             System.out.println(hasily);
 
